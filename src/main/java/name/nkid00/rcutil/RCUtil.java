626070e53eee27be9d9bd6b6945deb0773e86521
NKID00 <nkid00@pm.me>
2022-08-03 00:13:27 +0800

    refactor: dedicated server condition

--- src/main/java/name/nkid00/rcutil/RCUtil.java
+++ src/main/java/name/nkid00/rcutil/RCUtil.java
@@ -3,19 +3,23 @@ package name.nkid00.rcutil;
 import name.nkid00.rcutil.command.Command;
 import name.nkid00.rcutil.helper.Log;
 import name.nkid00.rcutil.storage.Storage;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class RCUtil implements ModInitializer {
-
-    public Storage data;
+    public static boolean isDedicatedServer = false;
 
     @Override
     public void onInitialize() {
+        var loader = FabricLoader.getInstance();
+        isDedicatedServer = loader.getEnvironmentType() == EnvType.SERVER;
+
         // storage handler
         ServerLifecycleEvents.SERVER_STARTED.register(Storage::load);
         ServerLifecycleEvents.SERVER_STOPPING.register(server -> Storage.save());
--- src/main/java/name/nkid00/rcutil/mixin/LanguageMixin.java
+++ src/main/java/name/nkid00/rcutil/mixin/LanguageMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import name.nkid00.rcutil.DedicatedServerUtil;
+import name.nkid00.rcutil.RCUtil;
 
 @Mixin(Language.class)
 public abstract class LanguageMixin {
@@ -19,7 +19,7 @@ public abstract class LanguageMixin {
     private static void loadLanguageFileForDedicatedServer(InputStream inputStream,
             BiConsumer<String, String> entryConsumer) {
         Language.load(inputStream, entryConsumer);
-        if (DedicatedServerUtil.isDedicatedServer && !languageLoaded) {
+        if (RCUtil.isDedicatedServer && !languageLoaded) {
             InputStream languageFileStream = Language.class.getResourceAsStream("/assets/rcutil/lang/en_us.json");
             Language.load(languageFileStream, entryConsumer);
             languageLoaded = true;
